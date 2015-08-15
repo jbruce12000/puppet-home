e6cb15d58aaa3ebe8687f32e09df833962a91b82
Jason Bruce <jbruce12000@gmail.com>
2015-08-15 18:35:00 -0400

    add some params for user

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -1,5 +1,12 @@
 class users {
   user { 'jason':
     ensure => present,
+    home   => '/home/jason',
+    shell  => '/bin/bash',
+    passwd => '$1$syoOjm/C$z10QBDS8n9q/bSwdbgCnk0',
+    groups => ['adm','cdrom','sudo','dip','plugdev','lpadmin','sambashare','vboxusers'],
     }
   } 
+
+# to get the passwd for puppet
+# openssl passwd -1 
