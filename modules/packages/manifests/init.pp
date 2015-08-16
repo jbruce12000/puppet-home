217b9bc89074ba81f77b5103998452c539566a3d
Jason Bruce <jbruce12000@gmail.com>
2015-08-15 22:23:25 -0400

    added lots of modules

--- modules/packages/manifests/init.pp
+++ modules/packages/manifests/init.pp
@@ -1,8 +1,60 @@
 class packages {
 include apt
 
+# basics
+package { 'vim': }
+package { 'ubuntu-restricted-extras': }
+package { 'tofrodos': }
+package { 'checkinstall': }
+package { 'build-essential': }
+package { 'fdupes': }
+package { 'xclip': }
+package { 'glipper': }
+
+# password mgmt
+package { 'password-gorilla': }
+
+
+# virtualization
+package { 'virtualbox-nonfree': }
+
+# networking
+package { 'xchat': }
+package { 'etherape': }
+
+# graphics
+package { 'inkscape': }
+
+# music
+package { 'picard': }
+
+# games
+package { 'pysol': }
+package { 'gweled': }
+package { 'anagramarama': }
+
+# android
+package { 'android-tools-adb': }
+package { 'android-tools-fastboot': }
+
+# timekpr to limit computer time
+apt::ppa { 'ppa:mjasnik/ppa': }
+package { 'timekpr': }
+
+# browsers
+package { 'firefox': }
+package { 'adblock-plus': }
+
+# peerguardian and torrent
 apt::ppa { 'ppa:jre-phoenix/ppa': }
 package { ['pgld','pglcmd','pglgui']:
   ensure => present
   }
+package { 'rtorrent': }
+
+# dropbox
+package { 'dropbox': }
+# samba, todo config
+package { 'samba': }
+
 }
--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -4,9 +4,21 @@ class users {
     home     => '/home/jason',
     shell    => '/bin/bash',
     password => '$1$syoOjm/C$z10QBDS8n9q/bSwdbgCnk0',
-    groups   => ['adm','cdrom','sudo','dip','plugdev','lpadmin','sambashare','vboxusers'],
+    groups   => ['adm','cdrom', 'sudo','dip','plugdev',
+                 'lpadmin','sambashare','vboxusers'],
     }
-  } 
+
+  file { ['/home/jason/torrents',
+          '/home/jason/torrents/new',
+          '/home/jason/torrents/notyet',
+          '/home/jason/torrents/tmp',
+          '/home/jason/torrents/session',
+          '/home/jason/torrents/done',]:
+    ensure => present,
+    }
+  }
 
 # to get the passwd for puppet
-# openssl passwd -1 
+# openssl passwd -1
+
+
